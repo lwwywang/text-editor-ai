a015f2a6e0d2b4aeb43b2ab3d2c71cfec89e8ac8
lwwywang <liwenwang4work@163.com>
2025-08-01 15:53:50 +0800

    fix: 修复 ESLint 配置问题并更新部署流程

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // 基本规则
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
+  },
+} 
